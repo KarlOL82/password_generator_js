2390013496cafc8230022df4714010b05b0bf0c6
KarLOL82 <klinfeldt@gmail.com>
2022-06-10 22:42:36 -0700

    added comments to js

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # password_generator_js
-A live page that generates a random unique password based on inputted user criteria.
+A live page that generates a random unique password based on criteria input by user.
--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // 
 
 var generateBtn = document.querySelector("#generate.btn");
-
+// Arrays of available characters
 var special = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
 var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -12,6 +12,8 @@ var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N
 
 
 function generatePassword() {
+
+  //define and validate length of password
  var passwordLength = parseInt( prompt("Choose password length. Must be 8 to 128 characters"));
     if (passwordLength < 8 || passwordLength > 128) {
       alert("Invalid entry. Please choose between 8 and 128 characters")
@@ -23,7 +25,7 @@ if (Number.isNaN(passwordLength)) {
   
 return; }
 
-
+// Gives user popup messages to select password criteria
 var specialCharacter = confirm("Click OK to include special characters");
 
 var numericCharacter = confirm("Click ok to include numbers 0-9");
@@ -31,12 +33,12 @@ var numericCharacter = confirm("Click ok to include numbers 0-9");
 var lowerCharacter = confirm("Click ok to include lower case letters");
 
 var upperCharacter = confirm("Click ok to include capital letters");
-// Assignment Code
 
+// stores characters from selected arrays
 var resultArray = [];
 
 var userCharacter = [];
-
+// code selects characters from the selected arrays
 if (specialCharacter) {
   userCharacter = userCharacter.concat(special)
   resultArray.push(special[Math.floor(Math.random()*special.length)])
@@ -64,10 +66,10 @@ for (var i = 0; i < remainingCharacters; i++){
 resultArray.push(userCharacter[Math.floor(Math.random()*userCharacter.length)])
 }
 
-
+//
 return resultArray.join("")
 
-}
+} 
 
 // Write password to the #password input
 function writePassword() {
