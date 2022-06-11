13b04b8a66dca83f0a7904fb4320eef4fbb440b0
KarLOL82 <klinfeldt@gmail.com>
2022-06-10 17:37:17 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,91 @@
+// What sort of data does my application need to work?
+
+// Storing all needed characters in variables
+
+// store 4 types of characters in variables (arrays): numbers, special characters, upper case and lower case
+
+// what sorts of actions does my code need to complete?
+
+// storing all needed characters in variables
+
+//numbers
+// var numbers = "1,2,3,4,5,6,7,8,9,0".split
+
+// Generate Password
+//function generatepassword () {
+function generatePassword() {
+
+if( true ) {
+
+  return "";
+}
+
+//  }
+}
+
+var variableA = false;
+
+var variableB = false;
+
+var variableC = false;
+
+var variableD = false;
+
+if( !variableA && ! !variableB) {
+  return "";
+
+
+}
+
+var myArray = [];
+
+var things = [1,2,4];
+var things2 = [1,2,3,5];
+
+// if "special character" is true
+// combine 
+
+myArray = myArray.concat(things)
+
+// if "numbers" is true:
+
+myarray = myArray.concat(things);
+//prompt the user for their passwordLength and store it in a variable 8 to 128 characters
+
+// prompt the user if they want special characters and store it in a variable
+
+// validate that at least one character choice is true
+
+// DECLARE a new empty array to store the characters we want to use
+
+if() {
+  return "";
+}
+
+// randomly select characters
+
+
+
+// validate the password length 8 to 128 characters
+
+// If "passwordLength" >= 8 and <= 128
+
+// POPUP BOXES js PROMPTS, ALERTS, and CONFIRM
+
+
+// see@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
